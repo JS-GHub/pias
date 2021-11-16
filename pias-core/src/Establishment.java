14ae532ef3628756ea24ca399f6fb55eeb4cc242
JS <joaohsereno@tecnico.ulisboa.pt>
2021-11-16 21:19:14 +0000

    Added Establishment class

---
+++ pias-core/src/Establishment.java
@@ -0,0 +1,9 @@
+package pias;
+
+import java.util.*;
+
+public class Establishment {
+  private ClientManager _clientManager = new ClientManager();
+  private OperationManager _operationManager = new OperationManager();
+  private List<ProductCategory> _categories = new ArrayList<>();
+}

27f471c313cef25b96e0340a99e5d3337c1764ac
JS <joaohsereno@tecnico.ulisboa.pt>
2021-11-17 12:29:37 +0000

    Package location fix

--- pias-app/src/app/Main/Label.java
+++
@@ -1,5 +0,0 @@
-package pias.app.main;
-
-interface Label {
-  String TITLE = "Menu Principal";
-}
--- pias-app/src/app/Main/Menu.java
+++
@@ -1,9 +0,0 @@
-package pias.app.main;
-
-import pias.EstablishmentManager;
-
-public class Menu extends pt.tecnico.uilib.menus.Menu {
-  public Menu(EstablishmentManager receiver) {
-    super(Label.TITLE);
-  }
-}

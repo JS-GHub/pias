d605419f624741972f5ed01e8f02c688e3990f4c
JS <joaohsereno@tecnico.ulisboa.pt>
2021-11-17 11:23:00 +0000

    Simple pias-app package

---
+++ pias-app/src/Main/Label.java
@@ -0,0 +1,5 @@
+package pias.app.main;
+
+interface Label {
+  String TITLE = "Menu Principal";
+}
---
+++ pias-app/src/Main/Menu.java
@@ -0,0 +1,9 @@
+package pias.app.main;
+
+import pias.EstablishmentManager;
+
+public class Menu extends pt.tecnico.uilib.menus.Menu {
+  public Menu(EstablishmentManager receiver) {
+    super(Label.TITLE);
+  }
+}
